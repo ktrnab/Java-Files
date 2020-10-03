a625e03af3cbc7581740b1adcb21b2166e3402bd
ktrnab <iamkatmlb@gmail.com>
2020-10-03 12:30:55 -0400

    Update PokerDice.java

--- PokerDice/PokerDice.java
+++ PokerDice/PokerDice.java
@@ -2,8 +2,6 @@
  *   A program that plays and scores a round of the game Poker Dice.  In this game,
  *   five dice are rolled.  The player is allowed to select a number of those five dice
  *   to re-roll.  The dice are re-rolled and then scored as if they were a poker hand.
- *   The following hands MUST be correctly scored in this assignment to receive
- *   full credit:
  *      * Highest value x
  *      * One pair
  *      * Two pair

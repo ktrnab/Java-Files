9140e41672d58db12fbe8eba27aab4b8c81ae3f0
ktrnab <iamkatmlb@gmail.com>
2020-10-07 19:56:55 -0400

    Update funwith.java

--- FunWithStrings/funwith.java
+++ FunWithStrings/funwith.java
@@ -1,6 +1,7 @@
 /**
  * FUN WITH STRINGS
- * @author KATRINA BUSGANO
+ * Practicing string methods
+ * @author KATRINA B
  * @version 20200120
  * 
  */
@@ -20,54 +21,40 @@ public class funwith {
         //Enter a long string
         System.out.print("Enter a long string: ");
         longString = scnr.nextLine(); // Gets entire line up to ENTER
-
-       
+     
         // Enter a substring
         System.out.print("Enter a substring: ");
         subString = scnr.nextLine(); // Gets entire line up to ENTER
-       
-       
-        
+               
         // Length of your string
         System.out.println("Length of your string: " + longString.length());
         
         // Length of your substring
         System.out.println("Length of your substring: " + subString.length());
-        
-        
+              
         // Starting position of your substring
         System.out.println("The starting position of your substring: " + longString.indexOf('j'));
-        
-        
-        
+              
         // String before your substring
         System.out.println("The string before your substring: " + longString.substring(0,19));
-        
-        
-        
+            
         //String after your substring
         System.out.println("The string after your substring: " + longString.substring(27,44));
-        
-        
+                
         //Enter a position between 0 and 43
         System.out.println("Enter a position between 0 and 43: ");
         stringPosition = scnr.nextInt();
-        
-        
+                
         //The character at position 
         characteratPosition = longString.charAt(stringPosition);
         System.out.println("The character at position " + stringPosition + " is " + characteratPosition);
-        
-        
+                
         //Enter a replacement string
         System.out.println("Enter a substring: ");
         replaceString = scnr.nextLine(); // Gets entire line up to ENTER
-        
-        
-        
+              
         //Your new string is
-        System.out.println(longString);
-       
+        System.out.println(longString); 
        
         //Goodbye
         System.out.println("Goodbye!");
